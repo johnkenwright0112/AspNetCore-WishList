298d4889cc8e9a82a9d95e43cae7f735111e14c2
johnkenwright0112 <52657403+johnkenwright0112@users.noreply.github.com>
2019-07-11 11:57:03 +0100

    step3

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items;
+
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+    }
+}
