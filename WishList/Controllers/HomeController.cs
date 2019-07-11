fe6d90fca7434a62cbffb0e3400b6fd162b74ef4
johnkenwright0112 <52657403+johnkenwright0112@users.noreply.github.com>
2019-07-11 11:11:14 +0100

    step 2

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
